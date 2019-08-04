3c2df47587c73e5b5b8c4240d6ab0e687196e9e4
alexladines <alexladines@outlook.com>
2019-08-03 21:52:40 -0700

    Add actual storyboard that will be used

---
+++ SpotSaver/View Controllers/LocationDetailsTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationDetailsTableViewController.swift
+//  SpotSaver
+//
+//  Created by Alex Ladines on 8/3/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsTableViewController: UITableViewController {
+    // MARK: - Properties
+
+    // MARK: - IBOutlets
+
+    // MARK: - IBActions
+
+    // MARK: - Life Cycle
+
+    // MARK: - Methods
+
+    // MARK: - Navigation
+
+    // MARK: - Data Persistance
+
+    // MARK: - UITableViewDataSource
+
+    // MARK: - UITableViewDelegate
+}
