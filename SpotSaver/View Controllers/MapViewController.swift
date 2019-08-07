e63f512318b7b8c77ad6eded83f4b53a7ce69ba9
alexladines <alexladines@outlook.com>
2019-08-06 18:52:51 -0700

    Add MapViewController

---
+++ SpotSaver/View Controllers/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  SpotSaver
+//
+//  Created by Alex Ladines on 8/6/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    // MARK: - Properties
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+
+    // MARK: - IBActions
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Methods
+
+    // MARK: - Navigation
+
+    // MARK: - Data Persistance
+
+}
