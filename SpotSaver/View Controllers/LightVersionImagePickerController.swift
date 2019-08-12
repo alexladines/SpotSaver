8a28983a13ba8b20b1937fa26928ac7514ed0ff5
alexladines <alexladines@outlook.com>
2019-08-12 12:49:44 -0700

    Subclass UIImagePickerController to allow light version of status bar

---
+++ SpotSaver/View Controllers/LightVersionImagePickerController.swift
@@ -0,0 +1,9 @@
+//
+//  LightVersionImagePickerController.swift
+//  SpotSaver
+//
+//  Created by Alex Ladines on 8/12/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import Foundation
